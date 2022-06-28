bbea8816eb9ca0c9faa9bcbdb0d4578d69a21ac3
Javier Sanz <javier.sanzbenede@telefonica.com>
2022-06-28 23:43:54 +0200

    Reducir tamaño del gradient

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  integrations: [preact()],
+  adapter: netlify(),
 });
